692ada49167b6beee9fb77d8421f4a3ae7b9a54f
VDiva <1806070444@qq.com>
2023-07-07 14:10:07 +0800

    添加监听客户端连接代码

--- NetWorkingServer/NetWorkingServer/Client.cs
+++ NetWorkingServer/NetWorkingServer/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace NetWorkingServer
 {
     public class Client
     {
-
+        public Socket socket;
+        public int ID;
     }
 }
--- NetWorkingServer/NetWorkingServer/NetWorking.cs
+++ NetWorkingServer/NetWorkingServer/NetWorking.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,20 +12,30 @@ namespace NetWorkingServer
     public class NetWorking
     {
         Socket socket;
-        SocketAsyncEventArgs saea;
+        SocketAsyncEventArgs saeaAccpet;
+        SocketAsyncEventArgs saeaReceive;
+        SocketAsyncEventArgs saeaSend;
 
+        ConcurrentStack<Client> clients;
+        List<Socket> OnlineClients;
+
+        public Action OnReceive;
         public NetWorking()
         {
-            
+            saeaAccpet = new SocketAsyncEventArgs();
+            saeaReceive = new SocketAsyncEventArgs();
+            saeaSend = new SocketAsyncEventArgs();
         }
-
         public void NetAsServer(string IP,int Port,int Num)
         {
-            saea = new SocketAsyncEventArgs();
-            saea.Completed += new EventHandler<SocketAsyncEventArgs>(Net_Comlpleted);
+            saeaAccpet = new SocketAsyncEventArgs();
+            saeaAccpet.Completed += new EventHandler<SocketAsyncEventArgs>(Accpet_Comlpleted);
             socket=new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(new IPEndPoint(IPAddress.Parse(IP), Port));
             socket.Listen(Num);
+            saeaReceive.SetBuffer(new byte[4096]);
+            clients=new ConcurrentStack<Client>();
+            OnlineClients = new List<Socket>();
             StartAccpet();
         }
 
@@ -37,27 +48,104 @@ namespace NetWorkingServer
         
         void StartAccpet()
         {
-            bool suspend = socket.AcceptAsync(saea);
+            bool suspend = socket.AcceptAsync(saeaAccpet);
             if (!suspend)
             {
-                ProcessConnect();
+                AccpetProcessConnect();
             }
         }
 
 
-        void ProcessConnect()
+        void AccpetProcessConnect()
         {
+            if(clients.Count>0&&clients.TryPop(out Client client))
+            {
+                client.socket = saeaAccpet.AcceptSocket;
+                OnlineClients.Add(saeaAccpet.AcceptSocket);
+            }
+            else
+            {
+                Client newClient= new Client();
+                newClient.socket = saeaAccpet.AcceptSocket;
+                OnlineClients.Add(saeaAccpet.AcceptSocket);
+            }
 
+            StartAccpet();
         }
 
 
-        void Net_Comlpleted(object sender, SocketAsyncEventArgs e)
+        void Accpet_Comlpleted(object sender, SocketAsyncEventArgs e)
         {
-            ProcessConnect();
+            AccpetProcessConnect();
             DebugLog.LogWarn("玩家IP" + (sender as Socket).RemoteEndPoint + "连上了服务器");
         }
 
-       
+
+
+
+
+
+        
+        void StartReceive()
+        {
+            bool suspend = socket.ReceiveAsync(saeaReceive);
+            if (!suspend)
+            {
+                ReceiveProcessConnect();
+            }
+        }
+
+
+        void ReceiveProcessConnect()
+        {
+            StartReceive();
+        }
+
+
+        void Receive_Comlpleted(object sender, SocketAsyncEventArgs e)
+        {
+
+            if (saeaReceive.BytesTransferred > 0 && saeaReceive.SocketError == SocketError.Success)
+            {
+                byte[] bytes = new byte[saeaReceive.BytesTransferred];
+                Buffer.BlockCopy(saeaReceive.Buffer, 0, bytes, 0, saeaReceive.BytesTransferred);
+                
+               
+                Socket s=sender as Socket;
+                OnlineClients.Remove(s);
+                s.Close();
+                clients.Push(new Client { socket = s });
+                
+            }
+            else
+            {
+                
+                DebugLog.LogError(saeaReceive.SocketError.ToString());
+            }
+
+            ReceiveProcessConnect();
+        }
+
+
+
+    
+
+
+        void SendProcessConnect()
+        {
+            
+        }
+
+
+        void Send_Comlpleted(object sender, SocketAsyncEventArgs e)
+        {
+
+            
+        }
+
+
+
+
 
     }
 }
